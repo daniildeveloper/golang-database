9126131e76074f4d410d1dfc07438876f2379b2b
daniiltserin <daniilborowkow@yandex.ru>
2017-09-14 18:03:59 +0600

    Base prompt init

--- main.go
+++ main.go
@@ -7,26 +7,56 @@ import (
 	"strings"
 )
 
+// InputBuffer description
 type InputBuffer struct {
 	buffer       string
-	bufferLength int32
-	inputLength  int32
+	bufferLength int
+	inputLength  int
 }
 
 func main() {
-	fmt.Printf("Hello")
+	input := createBuffer()
+
+	for true {
+		printPromt()
+
+		readInput(&input)
+
+		if strings.Compare(input.buffer, ".exit") == 0 {
+			os.Exit(1)
+		}
+	}
 }
 
-func readInput(inputBuffer InputBuffer) {
+func readInput(inputBuffer *InputBuffer) {
 	reader := bufio.NewReader(os.Stdin)
 	text, _ := reader.ReadString('\n')
-	text = strings.Replace(text, "\n", "", -1)
-}
+	// text = strings.TrimSpace()
+	text = strings.TrimRight(text, "\r\n")
+	inputBuffer.buffer = text
 
-func createBuffer() InputBuffer {
-	ib := InputBuffer{
-	// buffer = fmt.Fscanf()
+	if len(text) <= 0 {
+		panic("Error reading input")
 	}
 
+	inputBuffer.bufferLength = len(text)
+	inputBuffer.buffer = text
+}
+
+// create new buffer
+func createBuffer() InputBuffer {
+	ib := InputBuffer{
+		buffer:       "",
+		bufferLength: 0,
+		inputLength:  0}
+
 	return ib
+}
+
+func printPromt() {
+	fmt.Println("db > ")
+}
+func printStringInfo(str string) {
+	fmt.Printf("String: %s\n", str)
+	fmt.Printf("Length: %d\n", len(str))
 }
